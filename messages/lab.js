8f1fb50816da7a35b45c7557c1b994554233955b
John Casey <john_j.casey@tufts.edu>
2015-10-22 11:04:13 -0400

    First attempt

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+function parse() {
+	var xmlhttp = new XMLHttpRequest();
+	var url = 'data.json';
+	xmlhttp.onreadystateschange = function() {
+		if (xml.htp.readyState == 4 && xmlhttp.status == 200) {
+			var messages = JSON.parse(xmlhttp.responseText);
+			output(messages);
+		}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+function output(messages) {
+	var output = '';
+	for (i = 0; i < messages.length; i++) {
+		output = output + '<p>' + messages.i.content + '</p>';
+	}
+	document.getElementById("messages").innerHTML = output;
+}
+
+function parse1() {
+	document.getElementById("messages").innerHTML = '<p>testgood</p>';
+}
+
